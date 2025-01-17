f24eb06d5165255f154982decacac7c93a76295c
= <=>
2025-01-17 11:54:59 +0300

    raise ValueError if minutes are not within 0-59

--- working.py
+++ working.py
@@ -2,19 +2,24 @@ from re import match, IGNORECASE, search
 
 def convert(hours: str)-> str:
 
-    pattern = r'^(?P<begining>(?P<begin_hour>[0-9]|([1]{1}[1-2])){1}(?::(?P<begin_minutes>[0-9]*))?\s(?P<begining_meridiem>AM|PM){1})\sto\s'\
-              r'(?P<ending>(?P<ending_hour>[0-9]|(?:[1]{1}[1-2]))){1}(?::(?P<ending_minutes>[0-9]*))?\s(?P<ending_meridiem>AM|PM){1}$'
+    pattern = r'^(?P<begin_hour>[0-9]*)(?::(?P<begin_minutes>[0-9]*))?\s(?P<begining_meridiem>AM|PM){1}\sto\s'\
+              r'(?P<ending_hour>[0-9]*)(?::(?P<ending_minutes>[0-9]*))?\s(?P<ending_meridiem>AM|PM){1}$'
 
     time_match = match(pattern=pattern, string=hours, flags=IGNORECASE)
     if time_match:
-        begin_hour = time_match.group('begin_hour')
-        begin_minutes = time_match.group('begin_minutes')
+        begin_hour = int(time_match.group('begin_hour'))
+        begin_minutes = int(time_match.group('begin_minutes'))
         begin_meridiem = time_match.group('begining_meridiem')
-        endind_hour = time_match.group('ending_hour')
-        ending_minutes = time_match.group('ending_minutes')
+        endind_hour = int(time_match.group('ending_hour'))
+        ending_minutes = int(time_match.group('ending_minutes'))
         ending_meridiem = time_match.group('ending_meridiem')
     
+    if begin_hour and begin_hour < 0 and begin_hour > 12:
+        raise ValueError
+    
     if begin_minutes and begin_minutes < 0 and begin_minutes > 59:
+        raise ValueError
+    if ending_minutes and ending_minutes < 0 and ending_minutes > 59:
         raise ValueError
 
 
